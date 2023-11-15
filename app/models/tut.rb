8517f31951b8791536ca42541b65206cf0925818
Mohammed Raihan <121373366+rai5han@users.noreply.github.com>
2023-11-15 22:53:38 +0530

    by raihan

--- app/models/tut.rb
+++ app/models/tut.rb
@@ -1,3 +1,10 @@
 class Tut < ApplicationRecord
+  acts_as_list
   belongs_to :course
+  has_one_attached :video
+
+  def user_has_access?(user)
+    user.payments.exists?(course_id: self.course_id)
+  end
+  
 end
