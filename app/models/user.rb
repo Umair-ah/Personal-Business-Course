9a97a236905a311e61735563426dd988033bab59
shashank77889 <149724510+shashank77889@users.noreply.github.com>
2023-11-15 15:29:56 +0530

    by shashank

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,27 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :courses
+
+  has_many :payments # user can create many Payments
+  has_many :access_to_courses, through: :payments, source: :course # user have access to Course through payments only
+
+  pay_customer stripe_attributes: :stripe_attributes
+
+
+
+
+  def stripe_attributes(pay_customer)
+    {
+      address: {
+        city: pay_customer.owner.city,
+        country: pay_customer.owner.country
+      },
+      metadata: {
+        pay_customer_id: pay_customer.id,
+        user_id: id # or pay_customer.owner_id
+      }
+    }
+  end
 end
