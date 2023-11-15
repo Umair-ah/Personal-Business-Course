08c2f53fe6c7985e468e01efa27f54ff23f4b7c3
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-11-15 08:56:56 +0530

    all necessary routes needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "courses#index"
+  get "/my-courses", to: "statics#home"
+
+  resources :courses do
+
+    resources :payments do
+      collection do
+        get :success
+      end
+    end
+
+    resources :tuts do
+      member do 
+        delete :remove_video
+        patch :update_position
+      end
+    end
+  end
 end
