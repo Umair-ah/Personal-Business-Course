6e483477c5b8ae165cf7dba4035ef0be27130954
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-11-18 17:06:12 +0530

    drag and sort feature and UI changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def active?(path)
+    if current_page?(path)
+      "active"
+    end
+  end
 end
---
+++ app/javascript/controllers/sortable_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus";
+
+import Sortable from "stimulus-sortable";
+
+export default class extends Sortable {
+  connect() {
+    super.connect();
+    console.log("Do what you want here.");
+
+    // The sortable.js instance.
+    this.sortable;
+
+    // Your options
+    this.options;
+
+    // Your default options
+    this.defaultOptions;
+  }
+
+  // You can override the `onUpdate` method here.
+  onUpdate(event) {
+    super.onUpdate(event);
+  }
+
+  // You can set default options in this getter for all sortable elements.
+  get defaultOptions() {
+    return {
+      animation: 500,
+    };
+  }
+}
