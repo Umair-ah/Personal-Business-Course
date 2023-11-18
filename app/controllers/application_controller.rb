3455d9a5ff20a428e183de3f65d8ee54097fce67
bjornironside20 <150018703+bjornironside20@users.noreply.github.com>
2023-11-18 15:17:25 +0530

    Add files via upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 require 'stripe'
-Stripe.api_key = "your-api-key"
+Stripe.api_key = "sk_test_51NcIGEJdM2t98eyhyW2R6HDffCWMy4msgF16bpW3Mi20ihKOgpoPItOne2lVDSmqxUDZ4ehVJLnqAYErZvimN46h00fYFnRiOL"
 
 class ApplicationController < ActionController::Base
 end
