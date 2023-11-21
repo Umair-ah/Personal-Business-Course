b420194c640f8a90e6081192efbee8e9a843aae0
Rizwan <121442260+mdrizwan211@users.noreply.github.com>
2023-11-21 08:13:36 +0530

    Add files via upload

--- app/controllers/tuts_controller.rb
+++ app/controllers/tuts_controller.rb
@@ -2,11 +2,14 @@ require 'stripe'
 Stripe.api_key = "sk_test_51NcIGEJdM2t98eyhyW2R6HDffCWMy4msgF16bpW3Mi20ihKOgpoPItOne2lVDSmqxUDZ4ehVJLnqAYErZvimN46h00fYFnRiOL"
 
 class TutsController < ApplicationController
-  # These methods are called at the start of every REQUEST (i.e POST, GET, PATCH, DELETE)
   before_action :set_course, except: %i[remove_video show]
   before_action :set_tut, only: %i[edit remove_video update show destroy]
   before_action :authenticate_user!, only: %i[show]
-  # GET Request
+
+  def remove_video
+   
+  end
+  
   def index
     @tuts = @course.tuts.order(:position)
     if current_user
@@ -22,11 +25,11 @@ class TutsController < ApplicationController
       end
     end
   end
-# GET Request
+
   def new
     @tut = @course.tuts.build
   end
-# POST Request
+
   def create
     @tut = @course.tuts.build(tut_params)
     if @tut.save
@@ -36,26 +39,24 @@ class TutsController < ApplicationController
       render :new
     end
   end
-# GET Request
+
   def edit
 
   end
 
-  # PATCH Request
+  
   def update
     if @tut.update(tut_params)
       redirect_to course_tuts_path(@course)
     end
   end
 
-  # PATCH Request which dynamically changes the position of the video while sorting (Admin feature)
   def update_position
     @tut = Tut.find(params[:id])
     @tut.insert_at(tut_params[:position].to_i)
     head :ok
   end
 
-  # DELETE Request
   def destroy
     if @tut.destroy
       redirect_to course_tuts_path(@course), notice: "Video Deleted!"
@@ -63,7 +64,6 @@ class TutsController < ApplicationController
   end
   
 
-  # GET Request
 
   def show
     unless @tut.user_has_access?(current_user) || current_user.try(:type)
@@ -81,7 +81,7 @@ class TutsController < ApplicationController
     def set_tut
       @tut = Tut.find(params[:id])
     end
-# Permitting the formdetails into database
+
     def tut_params
       params.require(:tut).permit(:course_id, :title, :video, :position)
     end
